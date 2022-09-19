9b4438b78099ace809bd5b8831f578aceef0a7dc
Nguyen Cong Vien <congvien.01@gmail.com>
2022-09-19 16:11:46 +0700

    first commit

---
+++ src/main/java/edu/poly/shop/domain/CartItem.java
@@ -0,0 +1,29 @@
+package edu.poly.shop.domain;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "cartitems")
+public class CartItem {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int productId;
+	@Column(columnDefinition = "nvarchar(100) not null")
+	private String name;
+	@Column(nullable = false)
+	private int quantity;
+	@Column(nullable = false)
+	private double unitPrice;
+}
---
+++ src/main/java/edu/poly/shop/model/OrderDetail.java
@@ -0,0 +1,26 @@
+package edu.poly.shop.model;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class OrderDetail implements Serializable {
+	private int orderDetailId;
+	private int orderId;
+	private int productId;
+	private int quantity;
+	private double unitPrice;
+	
+}
